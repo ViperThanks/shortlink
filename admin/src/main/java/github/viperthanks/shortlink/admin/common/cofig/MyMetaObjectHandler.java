0c3a48c3b548614577932c785b7c21fa294c6901
Viperthanks <weilin@ufan.cn>
2024-02-15 21:21:42 +0800

    feat：通用实体填充

---
+++ admin/src/main/java/github/viperthanks/shortlink/admin/common/cofig/MyMetaObjectHandler.java
@@ -0,0 +1,30 @@
+package github.viperthanks.shortlink.admin.common.cofig;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.function.Supplier;
+
+/**
+ * desc:
+ *
+ * @author Viper Thanks
+ * @since 15/2/2024
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    private static final Supplier<Integer> ZERO_SUPPLIER = () -> 0;
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "del_flag", ZERO_SUPPLIER, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+}
