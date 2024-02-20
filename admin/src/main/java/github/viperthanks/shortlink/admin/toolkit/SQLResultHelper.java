f0fb37200a76a5a1e97b1425bf2006146e4394a7
Viperthanks <weilin@ufan.cn>
2024-02-20 17:16:51 +0800

    fix：工具类实现

--- admin/src/main/java/github/viperthanks/shortlink/admin/toolkit/SQLResultHelper.java
+++ admin/src/main/java/github/viperthanks/shortlink/admin/toolkit/SQLResultHelper.java
@@ -8,8 +8,9 @@ package github.viperthanks.shortlink.admin.toolkit;
  */
 public final class SQLResultHelper {
 
-    private static final int DML_SUC_MIN_ROW = 1;
+    private static final int DML_SUC_MIN_ROW = 0;
     private static final int DQL_SUC_MIN_ROW = 0;
+    private static final int COUNT_MIN = 0;
 
     /**
      * 是否是无效的DML语句
@@ -29,13 +30,20 @@ public final class SQLResultHelper {
      * 是否是有效的DML语句
      */
     public static boolean islLegalDMLResult(int effectRow) {
-        return effectRow >= DML_SUC_MIN_ROW;
+        return effectRow > DML_SUC_MIN_ROW;
     }
 
     /**
      * 是否是有效的DQL语句
      */
     public static boolean isLegalDQLResult(int effectRow) {
-        return effectRow >= DQL_SUC_MIN_ROW;
+        return effectRow > DQL_SUC_MIN_ROW;
+    }
+
+    /**
+     * 是否是有效的DQL语句
+     */
+    public static boolean isLegalCountResult(long count) {
+        return count > COUNT_MIN;
     }
 }
