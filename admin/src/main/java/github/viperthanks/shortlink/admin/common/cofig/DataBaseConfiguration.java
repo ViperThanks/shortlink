ee9b0497c00f1aa53f0efb8dcc8eb76e4f082424
Viperthanks <weilin@ufan.cn>
2024-02-25 20:15:44 +0800

    feature：Mysql数据库分页插件

---
+++ admin/src/main/java/github/viperthanks/shortlink/admin/common/cofig/DataBaseConfiguration.java
@@ -0,0 +1,26 @@
+package github.viperthanks.shortlink.admin.common.cofig;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * desc:
+ *
+ * @author Viper Thanks
+ * @since 25/2/2024
+ */
+@Configuration
+public class DataBaseConfiguration {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+    }
+}
