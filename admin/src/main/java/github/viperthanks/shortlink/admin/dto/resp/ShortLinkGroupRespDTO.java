608111594027dfa57ae3dcd3a6b0058d2c710b9f
Viperthanks <weilin@ufan.cn>
2024-02-27 16:12:22 +0800

    feature：修改查询短链接分组的接口返回值对象

--- admin/src/main/java/github/viperthanks/shortlink/admin/dto/resp/ShortLinkGroupRespDTO.java
+++ admin/src/main/java/github/viperthanks/shortlink/admin/dto/resp/ShortLinkGroupRespDTO.java
@@ -22,11 +22,6 @@ public class ShortLinkGroupRespDTO {
     private String name;
 
     /**
-     * 创建组织的用户名
-     */
-    private String username;
-
-    /**
      * 分组排序
      */
     private Integer sortOrder;
