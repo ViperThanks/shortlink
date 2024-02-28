02fc74739121ad2bb5ad310cbb65efab6b564e20
Viperthanks <weilin@ufan.cn>
2024-02-28 14:04:44 +0800

    feat：builder的实现

--- admin/src/main/java/github/viperthanks/shortlink/admin/dto/req/ShortLinkGroupSaveReqDTO.java
+++ admin/src/main/java/github/viperthanks/shortlink/admin/dto/req/ShortLinkGroupSaveReqDTO.java
@@ -1,6 +1,9 @@
 package github.viperthanks.shortlink.admin.dto.req;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * desc: 短链接组保存请求参数
@@ -9,10 +12,18 @@ import lombok.Data;
  * @since 19/2/2024
  */
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShortLinkGroupSaveReqDTO {
 
     /**
      * 分组名称
      */
     private String name;
+
+    /**
+     * 用户名
+     */
+    private String username;
 }
