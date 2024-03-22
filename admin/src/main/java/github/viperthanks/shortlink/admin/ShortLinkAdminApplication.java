57037feb4380927a14df29ddb2158952b878b312
Viperthanks <weilin@ufan.cn>
2024-03-22 15:29:59 +0800

    fix：恢复一下

--- admin/src/main/java/github/viperthanks/shortlink/admin/ShortLinkAdminApplication.java
+++ admin/src/main/java/github/viperthanks/shortlink/admin/ShortLinkAdminApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 
 /**
@@ -15,7 +14,6 @@ import org.springframework.context.ConfigurableApplicationContext;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = "github.viperthanks.shortlink..service")
 @MapperScan("github.viperthanks.shortlink.admin.dao.mapper")
 public class ShortLinkAdminApplication {
     public static void main(String[] args) {
