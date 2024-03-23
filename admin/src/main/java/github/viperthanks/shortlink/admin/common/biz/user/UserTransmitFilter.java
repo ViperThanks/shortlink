d995e5505717cd6d4747ee3d99b8bc49b09a92d8
Viperthanks <weilin@ufan.cn>
2024-03-23 19:51:34 +0800

    optimize ：优化后管用户登陆刷新时间

--- admin/src/main/java/github/viperthanks/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/github/viperthanks/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,81 +1,30 @@
 package github.viperthanks.shortlink.admin.common.biz.user;
 
-
-import com.alibaba.fastjson2.JSON;
-import github.viperthanks.shortlink.admin.common.constant.RedisCacheConstant;
-import github.viperthanks.shortlink.admin.common.convention.errorcode.BaseErrorCode;
-import github.viperthanks.shortlink.admin.common.convention.exception.ClientException;
-import github.viperthanks.shortlink.admin.common.convention.exception.ServiceException;
-import github.viperthanks.shortlink.admin.common.convention.result.Results;
-import github.viperthanks.shortlink.admin.common.enums.UserErrorCodeEnum;
-import jakarta.servlet.*;
+import cn.hutool.core.util.StrUtil;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ObjectUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.data.redis.core.StringRedisTemplate;
-
-import java.io.IOException;
-import java.util.Set;
-
-import static github.viperthanks.shortlink.admin.toolkit.WebUtil.returnJSON;
+import lombok.SneakyThrows;
 
 /**
- * desc: 用户信息传输过滤器
- *
- * @author Viper Thanks
- * @since 20/2/2024
+ * 用户信息传输过滤器
  */
 @RequiredArgsConstructor
-@Slf4j
 public class UserTransmitFilter implements Filter {
 
-    private final StringRedisTemplate stringRedisTemplate;
-
-    /**
-     * 白名单
-     */
-    private static final Set<String> WHITE_URLS = Set.of(
-            "/api/shortlink/admin/v1/user/login",
-            "/api/shortlink/admin/v1/user/has-username"
-    );
-
-
+    @SneakyThrows
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String username = httpServletRequest.getHeader("username");
-        String token = httpServletRequest.getHeader("token");
-        String requestURI = ((HttpServletRequest) servletRequest).getRequestURI();
-        if (WHITE_URLS.contains(requestURI)
-                || ("/api/shortlink/admin/v1/user".equals(requestURI) && "post".equalsIgnoreCase(httpServletRequest.getMethod()))) {
-            filterChain.doFilter(servletRequest, servletResponse);
-            return;
-        }
-        if (StringUtils.isAnyBlank(username, token)) {
-            returnJSON(servletResponse, JSON.toJSONString(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
-            return;
-        }
-        Object userDOJsonObj;
-        try {
-            userDOJsonObj = stringRedisTemplate.opsForHash().get(RedisCacheConstant.getCacheKey(RedisCacheConstant.LOGIN_USER_PREFIX, username), token);
-            if (ObjectUtils.isEmpty(userDOJsonObj)) {
-                returnJSON(servletResponse, JSON.toJSONString(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
-                return;
-            }
-        } catch (Exception e) {
-            returnJSON(servletResponse, JSON.toJSONString(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
-            return;
-        }
-        try {
-            UserInfoDTO userInfoDTO = JSON.parseObject(String.valueOf(userDOJsonObj), UserInfoDTO.class);
+        if (StrUtil.isNotBlank(username)) {
+            String userId = httpServletRequest.getHeader("userId");
+            String realName = httpServletRequest.getHeader("realName");
+            UserInfoDTO userInfoDTO = new UserInfoDTO(userId, username, realName);
             UserContext.setUser(userInfoDTO);
-        } catch (Exception e) {
-            log.error("解析时出错，username ： {}， token：{}", username, token, e);
-            UserContext.removeUser();
-            returnJSON(servletResponse, JSON.toJSONString(Results.failure(new ServiceException(BaseErrorCode.SERVICE_ERROR))));
-            return;
         }
         try {
             filterChain.doFilter(servletRequest, servletResponse);
@@ -83,6 +32,4 @@ public class UserTransmitFilter implements Filter {
             UserContext.removeUser();
         }
     }
-
-
 }
